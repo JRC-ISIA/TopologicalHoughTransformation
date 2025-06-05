92502bfe5164572e1cc7b3c30f0038a00c65aa1a
Martin Uray <martin.uray@fh-salzburg.ac.at>
2025-06-05 13:37:12 +0200

    added initial version of setup stuff

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="topological_hough_transform",
+    version="0.1.0",  # Initial version
+    author="Martin Uray",
+    author_email="martin.uray@fh-salzburg.ac.at",
+    description="A module implementing a topological variant of the Hough Transform for detecting lines in images.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/JRC-ISIA/TopologicalHoughTransformation",  # Replace with your repo URL
+    packages=find_packages(),  # Automatically find submodules
+    install_requires=[
+        "numpy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
