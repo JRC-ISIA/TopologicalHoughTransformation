0c1b4660cdca01d86c1d1271529c057271fa09d9
Martin Uray <martin.uray@fh-salzburg.ac.at>
2025-06-10 16:31:50 +0200

    fixed missing variable

--- utils/plotting.py
+++ utils/plotting.py
@@ -68,6 +68,7 @@ def draw_dashed_line(image, pt1, pt2, color=(0, 0, 255), thickness=2):
     - thickness: Thickness of the dashes.
     - dash_length: Length of each dash.
     """
+    dash_length = 20  # Length of each dash in pixels
     dist = np.linalg.norm(np.array(pt2) - np.array(pt1))
     vec = (np.array(pt2) - np.array(pt1)) / dist  # Direction vector
     num_dashes = int(dist // (2 * dash_length))  # Number of dashes
